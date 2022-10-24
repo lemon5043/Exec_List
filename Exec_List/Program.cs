0d8e27d53c24a019f1000de136743c9692813fe0
伯翰 侯 <john860703@gmail.com>
2022-10-24 16:47:31 +0800

    加入專案檔案。

---
+++ Exec_List/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_List
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			List<string> products = new List<string>() {"eggs", "bacon", "toast" };
+			foreach (var item in products)
+			{
+				Console.WriteLine(item);
+			} // print eggs bacon toast
+		}
+	}
+}
